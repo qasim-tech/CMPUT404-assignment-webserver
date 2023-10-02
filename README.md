d2ef4fc76eec9a64f36172a9d84397851fe2710e
Qasim Muhammad Akhtar <memon.qasim786@gmail.com>
2023-10-02 13:25:08 -0600

    added deep.png and root.png, changes to server.py

--- README.md
+++ README.md
@@ -24,3 +24,8 @@ examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
 
+References used
+========================
+https://www.tutorialspoint.com/http/http_responses.htm
+https://docs.python.org/3/library/socketserver.html
+https://www.geeksforgeeks.org/python-os-path-isdir-method/
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 #  coding: utf-8 
-import socketserver
+import socketserver, os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -26,13 +26,50 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+httpResponse = "HTTP/1.1 {} {}\r\nContent-Type: {}; charset=utf-8\r\n\r\n"
 
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        dataArray = self.data.decode().split(" ")    #split the request into an array
+
+        if dataArray[0] == "GET":
+            if "../" in dataArray[1]:
+                self.request.sendall(bytearray(httpResponse.format(404, "Not Found", "text/html")+"Page Not Found",'utf-8'))
+                return
+            
+            isDirectory = os.path.isdir("www" + dataArray[1]) #check if the path is a directory
+            isFile = os.path.isfile("www" + dataArray[1]) #check if the path is a file
+
+            if not isDirectory and not isFile: #if the path is neither a directory or a file, return 404
+                self.request.sendall(bytearray(httpResponse.format(404, "Not Found", "text/html")+"Page Not Found",'utf-8'))
+                return
+            
+            fileToRead = ""
+            if isDirectory:
+                if dataArray[1][-1] != "/":  
+                    self.request.sendall(bytearray("HTTP/1.1 301 Moved Permanently\r\nLocation: {}\nContent-Type: text/plain; charset=utf-8\r\n\r\n".format(dataArray[1]+"/"),'utf-8'))
+                    return
+                fileToRead = "www" + dataArray[1] + "index.html"
+            else:
+                fileToRead = "www" + dataArray[1]
+
+            f = open(fileToRead, "r")
+            file = f.read()
+
+            if ".html" in fileToRead:  #if the file is an html file, return 200 OK with text/html
+                self.request.sendall(bytearray(httpResponse.format(200, "OK", "text/html")+file,'utf-8'))
+                return
+            else: #if the file is a css file, return 200 OK with text/css
+                self.request.sendall(bytearray(httpResponse.format(200, "OK", "text/css")+file,'utf-8'))
+                return
+                
+        else:   
+            self.request.sendall(bytearray(httpResponse.format(405, "Method Not Allowed", "text/html"),'utf-8'))
+            return
+        
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
